1f18e7e3b1cbcf9beb43f8eae8ece481a6056b31
sttch <sidachen@live.com>
2018-04-10 11:08:12 -0400

    added runNameFilterTests

---
+++ app/src/test/java/com/example/jack/cs2340androidapp/runNameTests.java
@@ -0,0 +1,50 @@
+package com.example.jack.cs2340androidapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+/**
+ * Created by sidac on 4/10/2018.
+ */
+
+public class runNameTests {
+    private Shelter shelter;
+    private static final int TIMEOUT = 200;
+
+    @Before
+    public void setUp() {
+        shelter = new Shelter("random shelter", "500", "num", "n/a", "none");
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testNormal() {
+        //Reset filter to blank
+        Application.setFilter(new String[]{"", "", ""});
+        
+        //test normal cases
+        assertTrue(Application.runNameFilters(shelter));
+
+        //test capital letters
+        Application.setFilter(new String[]{"RANdom ShELter", "", ""});
+        assertTrue(Application.runNameFilters(shelter));
+
+        //test trim
+        Application.setFilter(new String[]{"   random shelter   ", "", ""});
+        assertTrue(Application.runNameFilters(shelter));
+
+        //test for loop
+        Application.setFilter(new String[]{"random shelter extra", "", ""});
+        assertFalse(Application.runNameFilters(shelter));
+
+        Application.setFilter(new String[]{"random shelters", "", ""});
+        assertFalse(Application.runNameFilters(shelter));
+
+        Application.setFilter(new String[]{"random shelter", "", ""});
+        assertTrue(Application.runNameFilters(shelter));
+    }
+
+}
